8e828ba7525c3e9d8cb48a1c951c48fea1ed73fc
pranav <pranavofficial404@gmail.com>
2022-11-22 10:29:24 +0530

    some changes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { User, Bookmark} from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+    constructor(private prismaService: PrismaService) { }
+    
    async login(): Promise<string>{
        return 'Helllo'
    }
