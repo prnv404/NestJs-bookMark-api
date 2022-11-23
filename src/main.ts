5f08656bb0c0c468e4b155e1271addc5d8f74732
pranav <pranavofficial404@gmail.com>
2022-11-23 16:25:05 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config'
+import { setFips } from 'crypto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
